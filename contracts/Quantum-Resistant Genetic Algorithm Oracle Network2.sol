49fecbe255ca983b9a8ea5e0963005b67999a6d4
bcasem1 <yearbca4@gmail.com>
2025-11-04 22:17:58 +0530

    Update Quantum-Resistant Genetic Algorithm Oracle Network2.sol

--- contracts/Quantum-Resistant Genetic Algorithm Oracle Network2.sol
+++ contracts/Quantum-Resistant Genetic Algorithm Oracle Network2.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol"; 
 
 /**
  * @title Quantum-Resistant Genetic Algorithm Oracle Network
@@ -289,3 +289,4 @@ contract QuantumOracle is Ownable, ReentrancyGuard {
         rewardPool += msg.value;
     }
 }
+
